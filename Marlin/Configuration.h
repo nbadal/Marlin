882429a0797b19cf0d0d6a46bc0400a453954dda
Nick Badal <nbadal@smugmug.com>
2020-12-26 16:47:25 -0800

    Bed and nozzle PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,9 +493,9 @@
     #define DEFAULT_Ki_LIST {   2.62,   2.62 }
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
-    #define DEFAULT_Kp  28.72
-    #define DEFAULT_Ki   2.62
-    #define DEFAULT_Kd  78.81
+    #define DEFAULT_Kp 37.48
+    #define DEFAULT_Ki 4.03
+    #define DEFAULT_Kd 87.13
   #endif
 #endif // PIDTEMP
 
@@ -534,9 +534,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi 85.47
-  #define DEFAULT_bedKd 624.59
+  #define DEFAULT_bedKp 135.12
+  #define DEFAULT_bedKi 26.34
+  #define DEFAULT_bedKd 462.11
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
